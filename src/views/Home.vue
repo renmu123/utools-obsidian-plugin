f8ade175165a6110ff9d3b17263422cbcb96e142
范颖玺 <1101022351@qq.com>
2021-11-02 10:38:34 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
+<!--  -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class=""></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import router from "../router";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  setup() {},
+  data() {
+    return {};
   },
+  created() {},
+  mounted() {
+    const utools = window.utools;
+
+    utools.onPluginReady(() => {
+      console.log("插件装配完成，已准备好");
+
+      // 插件第一次访问，如果vault为空，自动跳转至设置页
+      if (!utools.db.get("vault")) {
+        router.push("Setting");
+      }
+    });
+
+    utools.onPluginEnter(({ code, type, payload }) => {
+      console.log("用户进入插件", code, type, payload);
+      if (code === "ob-setting") {
+        router.push("Setting");
+      } else {
+        utools.setExpendHeight(0);
+        utools.setSubInput(({ text }) => {
+          document.addEventListener("keydown", (event) => {
+            if (event.code === "Enter") {
+              const encodeSearchWord = encodeURI(text);
+              const vault = utools.dbStorage.getItem("vault");
+              utools.shellOpenExternal(
+                `obsidian://search?vault=${vault}&query=${encodeSearchWord}`
+              );
+              utools.hideMainWindow();
+            }
+          });
+        }, "请输入需要搜索的文本");
+      }
+    });
+  },
+  computed: {},
+  watch: {},
+  methods: {},
 };
 </script>
+<style scoped></style>
---
+++ src/views/Setting.vue
@@ -0,0 +1,41 @@
+<!--  -->
+<template>
+  <form>
+    <div>
+      <label>库名称：</label
+      ><input v-model="form.vault" placeholder="请输入库名称" required />
+    </div>
+    <button @click="submit">提交</button>
+  </form>
+</template>
+
+<script>
+const utools = window.utools;
+
+export default {
+  components: {},
+  setup() {},
+  data() {
+    return {
+      form: {
+        vault: "",
+      },
+    };
+  },
+  created() {},
+  mounted() {
+    if (utools.dbStorage.getItem("vault")) {
+      this.form.vault = utools.dbStorage.getItem("vault");
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    submit() {
+      utools.dbStorage.setItem("vault", this.form.vault);
+      console.log(utools.dbStorage.getItem("vault"), this.form.vault);
+    },
+  },
+};
+</script>
+<style scoped></style>
