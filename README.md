b8dd54827482b9b59f9ce59772f0f2000b06fe74
范颖玺 <1101022351@qq.com>
2021-11-11 17:31:40 +0800

    更新UI&README

--- README.md
+++ README.md
@@ -1,24 +1,16 @@
-# utools-obsidian
+# 介绍
 
-## Project setup
-```
-npm install
-```
+通过 obsidian 的 uri 功能进行快速搜索
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+# 使用
 
-### Compiles and minifies for production
-```
-npm run build
-```
+安装完成后首先在 utools 窗口中输入 `ob-setting` 进行库的设置，库名称也是就“库文件夹”名称。
 
-### Lints and fixes files
-```
-npm run lint
-```
+PS: 如果有正确设置仍无法访问的情况请优先查询 obsidian 帮助文档中的 `使用 obsidian URI` 这篇文章，如果还是无法访问，请在 [issues](https://github.com/renmu123/utools-obsidian-plugin/issues) 进行反馈。
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+# 错误
+
+## 错误 1
+
+出现 `Vault not found`，请检查库设置是否正确。
+![](https://person-pic-1253385403.cos.ap-shanghai.myqcloud.com/img/20211111171028.png)
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,15 +1,25 @@
 <!--  -->
 <template>
-  <form>
-    <div>
-      <label>库名称：</label
-      ><input v-model="form.vault" placeholder="请输入库名称" required />
-    </div>
-    <button @click="submit">提交</button>
-  </form>
+  <el-form ref="form" :model="form" label-width="120px">
+    <el-form-item
+      label="库名称"
+      :rules="[{ required: true, message: '请填写库名称' }]"
+    >
+      <el-input
+        v-model="form.vault"
+        placeholder="请输入库名称"
+        required
+      ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submit">保存</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
+import { ElMessage } from "element-plus";
+
 const utools = window.utools;
 
 export default {
@@ -32,7 +42,19 @@ export default {
   watch: {},
   methods: {
     submit() {
-      utools.dbStorage.setItem("vault", this.form.vault);
+      if (this.form.vault === "") {
+        ElMessage.error("请填写库名称");
+      } else {
+        ElMessage({
+          message: "保存成功，你现在可以尝试搜索了，窗口将在3秒后关闭",
+          type: "success",
+        });
+        utools.dbStorage.setItem("vault", this.form.vault);
+        setTimeout(() => {
+          utools.hideMainWindow();
+        }, 3000);
+      }
+
       console.log(utools.dbStorage.getItem("vault"), this.form.vault);
     },
   },
